0a8264bc668e47563aa4ae9985e62ee0e14ebede
Antoine Patouillaud <antoine.patouillaud@gmail.com>
2019-02-13 17:06:05 +0100

    exo 2

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,37 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Dishoom',
+    address:      '2 rue de la paix',
+    category:     'italian',
+    phone_number:  '01 45 70 02 01'
+  },
+  {
+    name:         'Pitaya',
+    address:      '25 rue oberkampf ',
+    category:     'chinese',
+    phone_number:  '01 45 70 02 03'
+  },
+  {
+    name:         'Pizza del papa',
+    address:      '45 rue Giovani',
+    category:     'italian',
+    phone_number:  '01 45 70 02 02'
+  },
+  {
+    name:         'Les belles grenouilles',
+    address:      '2 rue de la paix',
+    category:     'french',
+  },
+  {
+    name:         'Leon de Bruges',
+    address:      '25 rue Montorgueil',
+    category:     'belgian',
+  },
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
